95c89e408c51f5e76bdcf950d8d8616f5598bc29
Kirill <kirill1991okt@gmail.com>
2022-01-17 20:45:53 +0300

    finish part 1

--- project/js/script.js
+++ project/js/script.js
@@ -1,61 +1,135 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту 
+
+*/
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
 
-const adv = document.querySelectorAll('.promo__adv img');
-const img = document.getElementsByTagName('img');
-const promoBg = document.querySelector('.promo__bg');
-const promoGenre = promoBg.querySelector('.promo__genre');
-const promoInteractiveList = document.querySelector('.promo__interactive-list');
-const promoTitle = document.querySelector('.promo__interactive-title');
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
-adv.forEach(item => item.remove());
+    const adv = document.querySelectorAll('.promo__adv img');
+    const promoBg = document.querySelector('.promo__bg');
+    const promoGenre = promoBg.querySelector('.promo__genre');
+    const promoInteractiveList = document.querySelector('.promo__interactive-list');
 
-promoBg.style.backgroundImage = 'url("img/bg.jpg")';
+    const addForm = document.querySelector('.add');
+    const addInput = addForm.querySelector('.adding__input');
+    const addCheck = addForm.querySelector('[type="checkbox"]');
 
-promoGenre.textContent = 'ДРАМА';
+    function removeElement(el) {
+        el.forEach(item => item.remove());
+    }
 
-promoInteractiveList.innerHTML = '';
+
+    function someChanges() {
+
+        promoBg.style.backgroundImage = 'url("img/bg.jpg")';
+
+        promoGenre.textContent = 'ДРАМА';
+    }
+
+    function sortArr(arr) {
+        arr.sort();
+    }
+
+    function createMovieList(movie, parent) {
 
 
-/*
+        parent.innerHTML = '';
 
-/ для отработки задания, сделал еще такой вариант
+        sortArr(movie);
 
-promoInteractiveList.remove();
+        movie.forEach((film, index) => {
+            parent.innerHTML += `<li class="promo__interactive-item">
+            ${index + 1} ${film}<div class="delete"></div></li>`;
+        });
 
-const ul = document.createElement('ul');
 
-ul.classList.add('promo__interactive-list');
 
-promoTitle.after(ul);
-*/
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movie.splice(i, 1);
 
-movieDB.movies.sort();
+                createMovieList(movie, parent);
 
-movieDB.movies.forEach((film, index) => {
-    promoInteractiveList.innerHTML += `<li class="promo__interactive-item">
-    ${index + 1} ${film}<div class="delete"></div></li>`;
+            });
+
+        });
+
+    }
+
+
+    /*
+
+    / для отработки задания, сделал еще такой вариант
+
+    promoInteractiveList.remove();
+
+    const ul = document.createElement('ul');
+
+    ul.classList.add('promo__interactive-list');
+
+    promoTitle.after(ul);
+    */
+
+
+    addForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let inputValue = addInput.value;
+        let checked = addCheck.checked;
+
+        if (inputValue) {
+
+            if (inputValue.length > 21) {
+                inputValue = `${inputValue.slice(0, 21)}...`;
+            }
+
+            movieDB.movies.push(inputValue);
+
+            sortArr(movieDB.movies);
+
+            createMovieList(movieDB.movies, promoInteractiveList);
+
+        } else {
+            alert('введите название фильма!!!');
+        }
+
+        if (checked && inputValue) {
+            console.log("Добавляем любимый фильм");
+        }
+
+        e.target.reset();
+
+    });
+
+    someChanges();
+    removeElement(adv);
+    createMovieList(movieDB.movies, promoInteractiveList);
+
 });
